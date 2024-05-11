2f4f52762e1cbeb645140764f67760109ec26fe7
pulkitgupta0 <pulkitgupta006@gmail.com>
2024-05-11 16:36:01 +0530

    final changes made

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,53 +3,17 @@ import { Link } from "react-router-dom";
 import "./login.css";
 const Login = () => {
   return (
-    <div>
-      <div id="pagee1">
-        <div id="left">
-          <h1>Synthomind</h1>
-          <div class="container">
-            <div class="form_area">
-              <p class="title">LOGIN</p>
-              <form action="">
-                <div class="form_group">
-                  <label class="sub_title" for="email">
-                    Email
-                  </label>
-                  <input
-                    placeholder="Enter your email"
-                    id="email"
-                    class="form_style"
-                    type="email"
-                  />
-                </div>
-                <div class="form_group">
-                  <label class="sub_title" for="password">
-                    Password
-                  </label>
-                  <input
-                    placeholder="Enter your password"
-                    id="password"
-                    class="form_style"
-                    type="password"
-                  />
-                </div>
-                <div>
-                  <button class="btn">LOGIN UP</button>
-                  <p>
-                    {" "}
-                    Don't Have Account?{" "}
-                    <Link to="/signup" class="link" href="">
-                      Sign Up
-                    </Link>
-                  </p>
-                  <a class="link" href=""></a>
-                </div>
-                <a class="link" href=""></a>
-              </form>
-            </div>
-            <a class="link" href=""></a>
-          </div>
-        </div>
+    <div className="wrapper">
+      <h1>Hello Again!</h1><br></br>
+      <p>Welcome back to SynthoMind</p>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+      
+      </form>
+      <button className="button11">Sign in</button>
+      <div className="not-member">
+        Not a member? <Link to="/signup">Register Now</Link>
       </div>
     </div>
   );
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -3,62 +3,19 @@ import "./signup.css";
 import { Link } from "react-router-dom";
 const Signup = () => {
   return (
-    <div>
-      <div id="pagee1">
-        <div id="left">
-          <h1>Synthomind</h1>
-          <div class="container">
-            <div class="form_area">
-              <p class="title">SIGN UP</p>
-              <form action="">
-                <div class="form_group">
-                  <label class="sub_title" for="name">
-                    Name
-                  </label>
-                  <input
-                    placeholder="Enter your full name"
-                    class="form_style"
-                    type="text"
-                  />
-                </div>
-                <div class="form_group">
-                  <label class="sub_title" for="email">
-                    Email
-                  </label>
-                  <input
-                    placeholder="Enter your email"
-                    id="email"
-                    class="form_style"
-                    type="email"
-                  />
-                </div>
-                <div class="form_group">
-                  <label class="sub_title" for="password">
-                    Password
-                  </label>
-                  <input
-                    placeholder="Enter your password"
-                    id="password"
-                    class="form_style"
-                    type="password"
-                  />
-                </div>
-                <div>
-                  <button class="btn">SIGN UP</button>
-                  <p>
-                    Have an Account?{" "}
-                    <Link to="/login" class="link" href="">
-                      Login Here!
-                    </Link>
-                  </p>
-                  <a class="link" href=""></a>
-                </div>
-                <a class="link" href=""></a>
-              </form>
-            </div>
-            <a class="link" href=""></a>
-          </div>
-        </div>
+    <div className="wrapper">
+      <h1>Welcome</h1><br/>
+      <p>Join our community</p>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="tel" placeholder="Phone no" />
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Password" />
+        
+      </form>
+      <button className="btn1">Sign Up</button>
+      <div className="not-member">
+        Already member? <Link to="/login">Login Now</Link>
       </div>
     </div>
   );
